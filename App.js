1ad0864d9f55a557ad0ed7f5fe5d9eb0da81cfc0
wilmarlopera <71187928+wilmarlopera@users.noreply.github.com>
2021-10-21 11:16:27 -0500

    se sube trabajo numero 1 react native completo

--- App.js
+++ App.js
@@ -1,12 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Picker } from 'react-native';
+
+//Esté comentario es una prueba.
 
 export default function App() {
+  const [vlrPres, setVlrPres] = React.useState("");
+  const [tipPres, setTipPres] = React.useState("");
+  const [numCuo, setNumCuo] = React.useState("");
+  const [vlrCuo, setVlrCuo] = React.useState("");
+  const [total, setTotal] = React.useState("");
+
+ 
+  const CalcularTotal = () => {
+   if(vlrPres != "" && tipPres != "" && numCuo != "" ){
+      if(parseFloat(vlrPres) > 4000000 ){
+       if(parseInt(numCuo)>0) {
+        setTotal(parseFloat(vlrPres)+(parseFloat(vlrPres)*parseFloat(tipPres)/100)*parseInt(numCuo));
+        setVlrCuo((parseFloat(vlrPres)+(parseFloat(vlrPres)*parseFloat(tipPres)/100)*parseInt(numCuo))/parseInt(numCuo));   
+       }  
+       else{
+         alert("el numero de cuotas debe ser mayor a 0");
+       }  
+      }
+      else{
+        alert("valor prestamo debe ser mayor a 4000000 millones");
+      }
+   }
+   else{
+     alert("todos los campos deben llenarse");
+   }
+  }
+  const Limpiar = () => {
+    setVlrPres('');
+    setTipPres('');
+    setNumCuo('');
+    setVlrCuo('');
+    setTotal('');
+ } 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Simulador de prestamo:</Text>
+      <br />
+      <br />
+      <Text style={styles.text}>Valor préstamo: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setVlrPres}
+        value={vlrPres}
+      />
+      <br />
+      <br />
+      
+      <Picker
+        selectedValue={tipPres}
+        stail={{height: 50 , widht: 200,borderWidht:1}}
+        onValueChange={(itemValue,itemIndex)=>setTipPres(itemValue)}
+      >
+        <Picker.Item label="seleccione el tipo de prestamo" value=""/>
+        <Picker.Item label="vivienda" value="0.5"/>
+        <Picker.Item label="educacion" value= "0.7"/>
+        <Picker.Item label="vehiculo" value="1.5"/>
+        <Picker.Item label="libre inversion" value="2.0"/>
+
+      </Picker>
+      
+      <br />
+      <br />
+      <Text style={styles.text}>Número cuotas: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setNumCuo}
+        value={numCuo}
+      />
+      <br />
+      <br />
+      <Text style={styles.text}>Valor cuota: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setVlrCuo}
+        value={vlrCuo}
+      />
+      
+      <br />
+      <br />
+      <Text style={styles.text}>Total deuda: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setTotal}
+        value={total}
+      />
+      <br />
+      <br />
+      <Button
+        title="Calcular"
+        onPress={CalcularTotal}
+      />
+      <br />
+      <Button
+        title="Limpiar campos"
+        onPress={Limpiar}
+      />
     </View>
   );
 }
@@ -18,4 +111,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+  input: {
+    border: "solid 1px black",
+    borderRadius: 10,
+    width: 200,
+    height: 40
+  },
+  text: {
+    fontWeight: 'bold'
+  }
 });
